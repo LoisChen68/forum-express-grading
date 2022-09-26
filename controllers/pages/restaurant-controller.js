e03289cea6eb3ddde5a758bd3cf57cc0a6dfb395
LoisChen68 <a8283626@gmail.com>
2022-09-27 02:02:13 +0800

    feat: add restaurant-services

--- controllers/pages/restaurant-controller.js
+++ controllers/pages/restaurant-controller.js
@@ -1,72 +1,10 @@
 const { Restaurant, Category, Comment, User } = require('../../models')
+const restaurantServices = require('../../services/restaurant-services')
 const { sequelize } = require('../../models')
 const { getUser } = require('../../helpers/auth-helpers')
-const { getOffset, getPagination } = require('../../helpers/pagination-helper')
 const restaurantController = {
   getRestaurants: (req, res, next) => {
-    const sortTab = [
-      {
-        name: '話題餐廳'
-      },
-      {
-        name: '熱門餐廳'
-      }
-    ]
-    const sortTabName = req.query.tab || ''
-    const DEFAULT_LIMIT = 9
-    const page = Number(req.query.page) || 1
-    const limit = Number(req.query.limit) || DEFAULT_LIMIT
-    const offset = getOffset(limit, page)
-    const categoryId = Number(req.query.categoryId) || ''
-    return Promise.all([
-      Restaurant.findAndCountAll({
-        include: [
-          Category,
-          Comment
-        ],
-        where: {
-          ...categoryId ? { categoryId } : {}
-        },
-        attributes: {
-          include: [
-            [
-              sequelize.literal(
-                '(SELECT COUNT(*) FROM Comments WHERE restaurant_id = Restaurant.id )'
-              ), 'CommentsCount'
-            ]
-          ]
-        },
-        order: [
-          [sequelize.literal('CommentsCount'), 'Desc']
-        ],
-        limit,
-        offset,
-        nest: true,
-        raw: true
-      }),
-      Category.findAll({ raw: true })
-    ])
-      .then(([restaurants, categories]) => {
-        const favoritedRestaurantsId = req.user && req.user.FavoritedRestaurants.map(fr => fr.id)
-        const likedRestaurantsId = req.user && req.user.LikedRestaurants.map(fr => fr.id)
-        const data = restaurants.rows.map(r => ({
-          ...r,
-          description: r.description.substring(0, 50),
-          isFavorited: favoritedRestaurantsId.includes(r.id),
-          isLiked: likedRestaurantsId.includes(r.id)
-        }))
-        const set = new Set()
-        const result = data.filter(item => !set.has(item.id) ? set.add(item.id) : false)
-        return res.render('restaurants', {
-          restaurants: result,
-          categories,
-          categoryId,
-          pagination: getPagination(limit, page, restaurants.count),
-          sortTab,
-          sortTabName
-        })
-      })
-      .catch(err => next(err))
+    restaurantServices.getRestaurants(req, (err, data) => err ? next(err) : res.render('restaurants', data))
   },
   getRestaurant: (req, res, next) => {
     const reqUser = getUser(req)
