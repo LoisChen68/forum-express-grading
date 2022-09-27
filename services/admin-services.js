89646e013bd7434e484b221749ae20c42b17f28c
LoisChen68 <a8283626@gmail.com>
2022-09-28 02:52:34 +0800

    feat: add create restaurant api

--- services/admin-services.js
+++ services/admin-services.js
@@ -1,4 +1,5 @@
 const { Restaurant, Category } = require('../models')
+const { imgurFileHandler } = require('../helpers/file-helpers')
 const adminServices = {
   getRestaurants: (req, cb) => {
     Restaurant.findAll({
@@ -9,6 +10,23 @@ const adminServices = {
 
       .then(restaurants => cb(null, { restaurants }))
 
+      .catch(err => cb(err))
+  },
+  postRestaurant: (req, cb) => {
+    const { name, tel, address, openingHours, description, categoryId } = req.body
+    if (!name) throw new Error('Restaurant name is required!')
+    const { file } = req
+    imgurFileHandler(file)
+      .then(filePath => Restaurant.create({
+        name,
+        tel,
+        address,
+        openingHours,
+        description,
+        image: filePath || null,
+        categoryId
+      }))
+      .then(newRestaurant => cb(null, { restaurant: newRestaurant }))
       .catch(err => cb(err))
   },
   deleteRestaurant: (req, cb) => {
